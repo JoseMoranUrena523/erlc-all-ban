e460bae250484e2517a63010e944317dd03eec9e
Jose Moran Urena <jose@josemoranurena.tech>
2024-07-04 10:21:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import fetch from 'node-fetch';
+import { QuickDB } from 'quick.db';
+const db = new QuickDB();
 
 const serverKey = 'exampleServerKey'; // Your server key, can be found in private server settings
 const baseURL = 'https://api.policeroleplay.community/v1/'; // Base URL, can be found at https://apidocs.policeroleplay.community/for-developers/api-reference
@@ -7,10 +9,12 @@ const interval = 6; // How often to check join logs (DO NOT CHANGE, RATE LIMIT)
 const banMessage = 'You are being banned for your username containing the word "all" in the beginning. This can disrupt mods from being able to do their job.'; // Message to be sent before being banned
 
 if (serverKey === 'exampleServerKey') {
-  return console.error("You've started the automation for the first time! Please set your server key in line 3 of index.js. You can also modify the interval, ban message, PRC's base URL, or the time frame between the private message and ban with lines 4-7.");
+  return console.error("You've started the automation for the first time! Please set your server key in line 3 of the script. You can also modify the interval, ban message, PRC's base URL, or the time frame between the private message and ban with lines 4-7.");
 }
 
 async function checkJoinLogs() {
+  await db.init();
+
   try {
     const response = await fetch(`${baseURL}server/joinlogs`, {
       headers: { 
@@ -29,6 +33,12 @@ async function checkJoinLogs() {
       if (/^all/i.test(player)) {
         const playerName = player.split(':')[0];
         const playerId = player.split(':')[1];
+
+        const isBanned = await db.get(playerId);
+        if (isBanned) {
+          console.log(`Player with ID ${playerId} is already banned.`);
+          return;
+        }
 
         try {
           await fetch(`${baseURL}server/command`, {
@@ -53,6 +63,9 @@ async function checkJoinLogs() {
                 command: `:ban ${playerId}`
               })
             });
+
+            await db.set(playerId, true);
+
           }, timeToBanAfterMessage * 1000);
         } catch (commandError) {
           console.error(`Error sending ban commands to player ${playerId}:`, commandError);
